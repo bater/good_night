e0c4ce44aa024368cb6d58cf5c355dc31675e137
bater <jace_ka@yahoo.com.tw>
2023-01-08 18:30:08 +0900

    Update seeds.rb for demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,6 @@
-User.create(name: 'Bater')
-User.create(name: 'Tom')
+user = FactoryBot.create(:user, :has_friend, :with_6_hours_sleep)
+first_friend = user.friendships.first
+FactoryBot.create(:sleep, user_id: first_friend.id)
+
+second_friend = FactoryBot.create(:user, :with_6_hours_sleep)
+user.follow(second_friend)
