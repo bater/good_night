ba9f870542aa1086286772af4401b87e7bfece93
bater <jace_ka@yahoo.com.tw>
2023-01-15 12:29:35 +0900

    Improve seed and mock data

--- spec/factories/sleeps.rb
+++ spec/factories/sleeps.rb
@@ -1,28 +1,29 @@
 FactoryBot.define do
   factory :sleep do
     association :user
-    wake_up { 1.day.ago + 8.hours }
-    created_at { 1.day.ago }
+    wake_up { 1.day.ago.end_of_day + 8.hours }
+    created_at { 1.day.ago.end_of_day }
     # 8 hours in seconds
     duration { 28800 }
 
     trait :six_hours do
-      wake_up { 1.day.ago + 6.hours }
+      wake_up { 1.day.ago.end_of_day + 6.hours }
       duration { 21600 }
     end
 
     trait :ten_hours do
-      wake_up { 1.day.ago + 10.hours }
+      wake_up { 1.day.ago.end_of_day + 10.hours }
       duration { 36000 }
     end
 
     trait :past_2_weeks do
-      wake_up { 2.weeks.ago + 8.hours }
-      created_at { 2.weeks.ago }
+      wake_up { 2.weeks.ago.end_of_day + 8.hours }
+      created_at { 2.weeks.ago.end_of_day }
     end
 
     trait :just_in_bed do
       wake_up { nil }
+      created_at { 1.day.ago }
       duration { 0 }
     end
   end
